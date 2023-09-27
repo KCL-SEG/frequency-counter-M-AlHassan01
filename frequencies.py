ec50e3b544a189367bc9de1931f106e4fec8d110
Miqdaad Al-Hassan <mm.al_hassan@outlook.com>
2023-09-27 14:25:39 +0100

    Complete frequencies(items) subroutine

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,12 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    """returns a dictionary containing the frequency in which an item in an input list appears"""
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        # Checks if an item exists in the dictionary
+        if frequencies.__contains__(str(item)):
+            frequencies[str(item)] += 1
+        else:
+            frequencies[str(item)] = 1
     return frequencies
